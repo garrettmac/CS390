a061deb31d939f85c6d71893f557b1eb5921f76b
garrettmac <garrettmacmaolain@gamil.com>
2014-10-01 01:19:04 -0600

    :x ;x

---
+++ Fragments/src/com/example/fragments/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.rbg;
+
+
+import android.app.Activity;
+import android.content.Intent;
+//import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
+import android.graphics.*;
+
+public class MainActivity extends Activity {
+
+	private static NumberPicker redNP = null;
+	private static NumberPicker greenNP = null;
+	private static NumberPicker blueNP = null;
+	private static ImageView drawingImageView = null;
+	private boolean intentFlag = false;
+	private Intent passedIntent;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.assignment1);
+
+		passedIntent = getIntent();
+		//Uri data = passedIntent.getData();
+
+		redNP = (NumberPicker) findViewById(R.id.redNumberPicker);
+		greenNP = (NumberPicker) findViewById(R.id.greenNumberPicker);
+		blueNP = (NumberPicker) findViewById(R.id.blueNumberPicker);
+
+		OnValueChangeListener colorValueChangeListener = new OnValueChangeListener() {
+			@Override
+			public void onValueChange(NumberPicker picker, int oldVal,
+					int newVal) {
+				Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+						.getDefaultDisplay().getWidth(),
+						(int) getWindowManager().getDefaultDisplay()
+								.getHeight(), Bitmap.Config.ARGB_8888);
+				Canvas canvas = new Canvas(bitmap);
+				drawingImageView.setImageBitmap(bitmap);
+
+				canvas.drawRGB(redNP.getValue(), greenNP.getValue(),
+						blueNP.getValue());
+			}
+		};
+
+		redNP.setMinValue(0);
+		redNP.setMaxValue(255);
+		greenNP.setMinValue(0);
+		greenNP.setMaxValue(255);
+		blueNP.setMinValue(0);
+		blueNP.setMaxValue(255);
+		
+		if (passedIntent != null) {
+			intentFlag = true;
+			redNP.setValue(Color.red(passedIntent.getIntExtra("CURRENT_COLOR", 0)));
+			greenNP.setValue(Color.green(passedIntent.getIntExtra("CURRENT_COLOR", 0)));
+			blueNP.setValue(Color.blue(passedIntent.getIntExtra("CURRENT_COLOR", 0)));
+	}
+
+		drawingImageView = (ImageView) this.findViewById(R.id.DrawingImageView);
+		Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+				.getDefaultDisplay().getWidth(), (int) getWindowManager()
+				.getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		drawingImageView.setImageBitmap(bitmap);
+
+		// Rectangle
+
+		Paint paint = new Paint();
+		paint.setColor(Color.rgb(redNP.getValue(), greenNP.getValue(),
+				blueNP.getValue()));
+		paint.setStyle(Paint.Style.FILL_AND_STROKE);
+		paint.setStrokeWidth(10);
+		float leftx = 0;
+		float topy = 0;
+		float rightx = bitmap.getWidth();
+		float bottomy = bitmap.getHeight();
+
+		canvas.drawRect(leftx, topy, rightx, bottomy, paint);
+
+		redNP.setOnValueChangedListener(colorValueChangeListener);
+		greenNP.setOnValueChangedListener(colorValueChangeListener);
+		blueNP.setOnValueChangedListener(colorValueChangeListener);
+
+		Log.i("Assignemtn1_OnCreate", "On create finished");
+
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+
+		passedIntent.putExtra(
+				"COLOR_PICKED",
+				Color.rgb(redNP.getValue(), greenNP.getValue(),
+						blueNP.getValue()));
+		setResult(Activity.RESULT_OK, passedIntent);
+		finish();
+	}
+
+	public void updateColor(View view) {
+		Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+				.getDefaultDisplay().getWidth(), (int) getWindowManager()
+				.getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		drawingImageView.setImageBitmap(bitmap);
+
+		canvas.drawRGB(redNP.getValue(), greenNP.getValue(), blueNP.getValue());
+
+		if (intentFlag) {
+			passedIntent.putExtra(
+					"COLOR_PICKED",
+					Color.rgb(redNP.getValue(), greenNP.getValue(),
+							blueNP.getValue()));
+			setResult(Activity.RESULT_OK, passedIntent);
+			finish();
+		}
+	}
+}
---
+++ RBG/src/com/example/rbg/IntentActivity.java
@@ -0,0 +1,178 @@
+package com.example.rbg;
+
+
+
+import android.app.*;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.*;
+import android.util.Log;
+import android.view.*;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+public class IntentActivity extends Activity {
+	final int LEFT = 0;
+	final int RIGHT = 1;
+	private Intent launchProjectIntent;
+	private View leftView;
+	private View rightView;
+	private View blendedView;
+	private SeekBar colorSlider;
+	private int leftColor;
+	private int rightColor;
+	private int blendedColor;
+	private colorSliderListener sliderListener = new colorSliderListener();
+	private int leftPercent = 100;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.colorblenderactivity);
+		Log.i("ColorBlenderActivity_OnCreate", "On create started");
+
+		leftView = findViewById(R.id.leftColor);
+		rightView = findViewById(R.id.rightColor);
+		blendedView = findViewById(R.id.blendedColor);
+		colorSlider = (SeekBar) findViewById(R.id.colorSeekBar);
+
+		colorSlider.setOnSeekBarChangeListener(sliderListener);
+		leftColor = getColor(leftView);
+		rightColor = getColor(rightView);
+		blendedColor = blendColor(0, leftColor, rightColor);
+		blendedView.setBackgroundColor(blendedColor);
+
+		Log.i("ColorBlenderActivity_OnCreate", "On create finished");
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		// setContentView(R.layout.colorblenderactivity);
+		Log.i("ColorBlenderActivity_onStart", "On start started");
+
+		/*
+		 * leftView.setBackgroundColor(leftColor);
+		 * rightView.setBackgroundColor(rightColor);
+		 * blendedView.setBackgroundColor(blendedColor);
+		 */
+		Log.i("ColorBlenderActivity_onStart", "On start finished");
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		int color = data.getIntExtra("COLOR_PICKED", 0);
+
+		if (resultCode != RESULT_OK) {
+			Log.i("ColorBlenderActivity_onActivityResult",
+					"ColorPicker returned error code:" + resultCode);
+			return;
+		}
+
+		switch (requestCode) {
+		case LEFT:
+			leftView.setBackgroundColor(color);
+			Log.i("ColorBlenderActivity_onActivityResult", "LEFT color set to:"
+					+ color);
+			break;
+		case RIGHT:
+			rightView.setBackgroundColor(color);
+			Log.i("ColorBlenderActivity_onActivityResult",
+					"RIGHT color set to:" + color);
+			break;
+		default:
+			Log.i("ColorBlenderActivity_onActivityResult",
+					"invalid requestCode recieved:" + requestCode);
+			break;
+		}
+
+		leftColor = getColor(leftView);
+		rightColor = getColor(rightView);
+		blendedColor = blendColor(100 - leftPercent, leftColor, rightColor);
+		blendedView.setBackgroundColor(blendedColor);
+	}
+
+	public void setRightColor(View view) {
+		launchProjectIntent = new Intent(this, assignment1Activity.class);
+		launchProjectIntent.putExtra("CURRENT_COLOR", rightColor);
+		startActivityForResult(launchProjectIntent, RIGHT);
+		Log.i("ColorBlenderActivity_setLeftColor", "setLeftColor finished");
+		Log.i("ColorBlenderActivity_setRightColor", "setRightColor finished");
+	}
+
+	public void setLeftColor(View view) {
+		launchProjectIntent = new Intent(this, assignment1Activity.class);
+		launchProjectIntent.putExtra("CURRENT_COLOR", leftColor);
+		startActivityForResult(launchProjectIntent, LEFT);
+		Log.i("ColorBlenderActivity_setLeftColor", "setLeftColor finished");
+	}
+
+	private int getColor(View view) {
+		ColorDrawable output = (ColorDrawable) view.getBackground();
+		return output.getColor();
+	}
+
+	private int blendColor(int rightPercent, int left, int right) {
+		/*
+		 * Log.i("ColorBlenderActivity_blendColor", "left:" + left);
+		 * Log.i("ColorBlenderActivity_blendColor", "right:" + right);
+		 */
+
+		int leftRed = Color.red(left);
+		int leftBlue = Color.blue(left);
+		int leftGreen = Color.green(left);
+		int rightRed = Color.red(right);
+		int rightBlue = Color.blue(right);
+		int rightGreen = Color.green(right);
+		leftPercent = 100 - rightPercent;
+
+		/*
+		 * Log.i("ColorBlenderActivity_blendColor", "leftRed:" + leftRed);
+		 * Log.i("ColorBlenderActivity_blendColor", "leftBlue:" + leftBlue);
+		 * Log.i("ColorBlenderActivity_blendColor", "leftGreen:" + leftGreen);
+		 * Log.i("ColorBlenderActivity_blendColor", "leftPercent:" +
+		 * leftPercent); Log.i("ColorBlenderActivity_blendColor", "rightRed:" +
+		 * rightRed); Log.i("ColorBlenderActivity_blendColor", "rightBlue:" +
+		 * rightBlue); Log.i("ColorBlenderActivity_blendColor", "rightGreen:" +
+		 * rightGreen); Log.i("ColorBlenderActivity_blendColor", "rightPercent:"
+		 * + rightPercent);
+		 */
+
+		return Color.rgb(
+				(leftRed * leftPercent + rightRed * rightPercent) / 100,
+				(leftGreen * leftPercent + rightGreen * rightPercent) / 100,
+				(leftBlue * leftPercent + rightBlue * rightPercent) / 100);
+	}
+
+	private class colorSliderListener implements OnSeekBarChangeListener {
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,
+				boolean fromUser) {
+			blendedColor = blendColor(progress, leftColor, rightColor);
+			blendedView.setBackgroundColor(blendedColor);
+			blendedView.invalidate();
+			/*
+			 * Log.i("ColorBlenderActivity_onProgressChanged",
+			 * "onProgressChanged finished");
+			 * Log.i("ColorBlenderActivity_onProgressChanged", "blendedColor:" +
+			 * blendedColor); Log.i("ColorBlenderActivity_onProgressChanged",
+			 * "progress:" + progress);
+			 */
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+			Log.i("ColorBlenderActivity_onStartTrackingTouch",
+					"onStartTrackingTouch finished");
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			Log.i("ColorBlenderActivity_onStopTrackingTouch",
+					"onStopTrackingTouch finished");
+		}
+	}
+
+}
